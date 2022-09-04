92bc9a93cd75927f1cd3662717076c6a89fcc97b
Justus B <justus.c.brown@gmail.com>
2022-09-04 10:57:38 -0700

    fix group creation route

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,138 +3,145 @@ const { User, Post, Group, Comment } = require('../models');
 module.exports = {
     getAllGroups(req, res) {
         Group.find()
-        .populate('posts')
-        .then((groups) => res.json(groups))
-        .catch((err) => {
-            console.log(err)
-            res.status(500).json(err)});
+            .populate('posts')
+            .then((groups) => res.json(groups))
+            .catch((err) => {
+                console.log(err)
+                res.status(500).json(err)
+            });
+    },
+    getAllUsersGroups(req, res) {
+
+        console.log(req.body.idList)
+        Group.findbyPk()
+            .populate('posts')
+            .then((groups) => res.json(groups))
+            .catch((err) => {
+                console.log(err)
+                res.status(500).json(err)
+            });
     },
 
     getOneGroup(req, res) {
         Group.findOne({ _id: req.body.GroupId })
-        .populate([
-			{
-				path:'posts',
-			},
-			{
-				path:'members',
-			},
-		])
-        .select('-__v')
-        .then((group) =>
-        !group
-          ? res.status(404).json({ message: 'No group with that ID' })
-          : res.json(group)
-      )
-      .catch((err) => res.status(500).json(err));
+            .populate([
+                {
+                    path: 'posts',
+                },
+                {
+                    path: 'members',
+                },
+            ])
+            .select('-__v')
+            .then((group) =>
+                !group
+                    ? res.status(404).json({ message: 'No group with that ID' })
+                    : res.json(group)
+            )
+            .catch((err) => res.status(500).json(err));
     },
 
     createNewGroup(req, res) {
         Group.create(req.body)
-        .then(async (groupData) => {
-			res.json(groupData)
-			const adminData = await User.findOneAndUpdate(
-				{ _id: groupData.admin },
-				{ $addToSet: { groups: { _id: groupData.id } } },
-				{ new: true }
-			);
-			return await Group.findOneAndUpdate(
-				{ _id: adminData.groups[adminData.groups.length - 1] },
-				{ $addToSet: { members: { _id: adminData._id } } },
-				{ new: true }
-			);
-		})
-        .catch((err) => res.status(500).json(err));
+            .then(async (groupData) => {
+                User.findOneAndUpdate(
+                    { _id: groupData.admin },
+                    { $addToSet: { groups: { _id: groupData.id } } },
+                    { new: true }
+                    );
+                    res.json(groupData)
+            })
+            .catch((err) => res.status(500).json(err));
     },
 
     updateGroup(req, res) {
         Group.findOneAndUpdate(
             { _id: req.body.GroupId },
             { $set: req.body },
-            {new: true}
+            { new: true }
         ).then((group) => {
-        !group
-          ? res.status(404).json({ message: 'No group with this id!' })
-          : res.json(group)
+            !group
+                ? res.status(404).json({ message: 'No group with this id!' })
+                : res.json(group)
         })
-      .catch((err) => res.status(500).json(err));
+            .catch((err) => res.status(500).json(err));
     },
 
     deleteGroup(req, res) {
         Group.findOneAndDelete({ _id: req.body.GroupId })
-      .then((group) =>
-        !group
-          ? res.status(404).json({ message: 'No group with that ID' })
-          : Post.deleteMany({ _id: { $in: group.posts } })
-      )
-      .then(() => res.json({ message: 'group and posts deleted!' }))
-      .catch((err) => res.status(500).json(err));
+            .then((group) =>
+                !group
+                    ? res.status(404).json({ message: 'No group with that ID' })
+                    : Post.deleteMany({ _id: { $in: group.posts } })
+            )
+            .then(() => res.json({ message: 'group and posts deleted!' }))
+            .catch((err) => res.status(500).json(err));
     },
 
     acceptRequest(req, res) {
         Group.findOneAndUpdate(
             { _id: req.body.GroupId },
-            { 
-				$addToSet: { members: req.body.UserId },
-				$pull: { inbox: req.body.UserId } 
-			},
+            {
+                $addToSet: { members: req.body.UserId },
+                $pull: { inbox: req.body.UserId }
+            },
         ).then((group) =>
-        !group
-          ? res.status(404).json({ message: 'No group with this id!' })
-          : res.json(group)
+            !group
+                ? res.status(404).json({ message: 'No group with this id!' })
+                : res.json(group)
         ).then(
-        User.findOneAndUpdate(
-            { _id: req.body.UserId },
-            { $addToSet: { groups: req.body.GroupId } },
-        )
-        .catch((err) => {
-        console.log(err)
-        res.status(500).json(err)
-    }));
+            User.findOneAndUpdate(
+                { _id: req.body.UserId },
+                { $addToSet: { groups: req.body.GroupId } },
+            )
+                .catch((err) => {
+                    console.log(err)
+                    res.status(500).json(err)
+                }));
     },
 
-	denyRequest(req, res) {
-		Group.findOneAndUpdate(
-		{ _id: req.body.GroupId },
-		{ $pull: { inbox: req.body.UserId } }
-		)
-		.then((user) =>
-			!user
-			? res.status(404).json({ message: "No user with this id!" })
-			: res.json(user)
-		)
-	},
-	
+    denyRequest(req, res) {
+        Group.findOneAndUpdate(
+            { _id: req.body.GroupId },
+            { $pull: { inbox: req.body.UserId } }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with this id!" })
+                    : res.json(user)
+            )
+    },
+
     deleteMember(req, res) {
         Group.findOneAndUpdate(
             { _id: req.body.GroupId },
             { $pull: { members: req.body.UserId } },
         ).then((group) =>
-        !group
-          ? res.status(404).json({ message: 'No group with this id!' })
-          : res.json(group)
+            !group
+                ? res.status(404).json({ message: 'No group with this id!' })
+                : res.json(group)
         ).then(
-        User.findOneAndUpdate(
-            { _id: req.body.UserId },
-            { $pull: { groups: req.body.GroupId } },
-        )
-        .catch((err) => {
-        console.log(err)
-        res.status(500).json(err)
-    }));
+            User.findOneAndUpdate(
+                { _id: req.body.UserId },
+                { $pull: { groups: req.body.GroupId } },
+            )
+                .catch((err) => {
+                    console.log(err)
+                    res.status(500).json(err)
+                }));
     },
 
-	deactivateGroup(req, res){
+    deactivateGroup(req, res) {
         Group.findOneAndUpdate(
             { _id: req.body.GroupId },
-            { $set: {isDeactivated:req.body.isDeactivated} },
-            { new:true},
+            { $set: { isDeactivated: req.body.isDeactivated } },
+            { new: true },
         ).then((group) => {
             console.log(group)
-        !group
-          ? res.status(404).json({ message: 'No group with this id!' })
-          : res.json(group)
-          })
-      .catch((err) => res.status(500).json(err));
+            !group
+                ? res.status(404).json({ message: 'No group with this id!' })
+                : res.json(group)
+        })
+            .catch((err) => res.status(500).json(err));
     },
 }
